e1f2bb858a946fd6c5b72461fc1fd1294344a7d6
Kelvin Wooldbridge <kelvinwooldbridge@gmail.com>
2022-10-14 09:39:48 -0700

    add 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,15 +10,17 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
 	if (n < 0)
 	{
 		printf("%d is negative", n);
-	}else if (n == 0)
+	} else if (n == 0)
 	{
 		printf("%d is zero", n);
-	}else
+	} else
 	{
 		printf("%d is positive", n);
 	}
