2f4d5423fbad5eaff21c5907936219f778b29ad9
Kelvin Wooldbridge <kelvinwooldbridge@gmail.com>
2022-10-14 09:22:58 -0700

    add 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always return (0) success
+ */
+
+int main(void)
+{
+	int n;
+	srand(time(0));
+
+	if (n < 0)
+	{
+		printf("%d is negative", n);
+	}else if (n == 0)
+	{
+		printf("%d is zero", n);
+	}else
+	{
+		printf("%d is positive", n);
+	}
+	return (0);
+}
