9caffad3906f14b254fbea9df1c978683838453f
Kelvin Wooldbridge <kelvinwooldbridge@gmail.com>
2022-10-14 11:06:26 -0700

    add 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry out
+ *
+ * Return: always (0)
+ */
+
+int main(void)
+{
+	char lower;
+
+	lower = 'a';
+
+	while (lower <= 'z')
+	{
+		putchar(lower);
+	}
+	putchar('\n');
+	return (0);
+}
