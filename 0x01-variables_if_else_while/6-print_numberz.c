790126d781e626b3b5c1166b45feaca353182fa4
Kelvin Wooldbridge <kelvinwooldbridge@gmail.com>
2022-10-14 13:22:38 -0700

    add 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry out
+ *
+ * Return: always (0)
+ */
+
+int main(void)
+{
+	int num;
+
+	num = 0;
+
+	while (num < 10)
+	{
+		putchar(num % +'0');
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
+
