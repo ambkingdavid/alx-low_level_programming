4ee9b5f5f6dd8f5fefa342f9a24ebeeb5e66d26c
Kelvin Wooldbridge <kelvinwooldbridge@gmail.com>
2022-10-18 06:32:44 -0700

    add 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	letter = 'a';
+
+	while (letter <= 'z')
+		_putchar(letter);
+	_putchar('\n');
+}
