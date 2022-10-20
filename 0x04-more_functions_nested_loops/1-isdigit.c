339ebea9d4399ff82ef24daaf1dcbdd74bbf826e
Kelvin Wooldbridge <kelvinwooldbridge@gmail.com>
2022-10-20 11:31:01 -0700

    add 1-isdigit.c file

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _isupper - Checks for uppercase characters.
+* @c: The character to be checked.
+*
+* Return: 1 if character is uppercase, 0 otherwise.
+*/
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
+}
