f130ec2c6ac041eff41360498e01c73654c8ba46
Kelvin Wooldbridge <kelvinwooldbridge@gmail.com>
2022-10-26 23:59:36 -0700

    add hot fix

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,18 +5,19 @@
   *
   * @dest: a pointer to the string operand
   * @src: a pointer to the string to concat with the operand.
+  * @n: size of byte of @src
   *
   * Return: A pointer to the destination string @dest.
   */
 
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
 	int index = 0, dest_len = 0;
 
 	while (dest[index++])
 		dest_len++;
 
-	for (index = 0; src[index]; index++)
+	for (index = 0; src[index] && index < n; index++)
 		dest[dest_len++] = src[index];
 
 	return (dest);
