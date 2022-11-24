b4a6bd2c3e197ef9608341382e2e865b3150c965
Kelvin Wooldbridge <kelvinwooldbridge@gmail.com>
2022-11-24 13:25:06 +0100

    add  function that prints all the elements of a list_t list.

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef LIST_H
-#define LIST_H
+#ifndef LISTS_H
+#define LISTS_H
 
 #include <stdio.h>
 
@@ -9,6 +9,7 @@
  * @len: length of the string
  * @next: points to the next node
  *
+ * Description:  singly linked list node structure.
  */
 
 typedef struct list_l
@@ -25,4 +26,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif /* LIST_h */
+#endif /* LISTS_H */
