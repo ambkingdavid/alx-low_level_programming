968b88be03b14e6779877626aeaa808fab911f9c
ambkingdavid <kelvinwooldbridge@gmail.com>
2022-11-29 12:59:17 +0100

    add task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,6 +25,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (head == NULL)
 		return (NULL);
+	while (tmp != NULL)
+		len++;
+	if (idx > len + 1)
+		return NULL;
 
 	if (*head == NULL)
 	{
