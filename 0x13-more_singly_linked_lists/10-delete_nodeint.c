a9fcea516ebc9f73b8854498f2b7e0880c5b97f9
ambkingdavid <kelvinwooldbridge@gmail.com>
2022-11-29 14:41:26 +0100

    add task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -19,7 +19,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (current == NULL)
 		return (-1);
 
-	if (index = 0)
+	if (index == 0)
 	{
 		*head = *head->next;
 		free(current);
