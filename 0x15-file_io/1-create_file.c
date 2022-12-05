cd74a0e4393ede26713559ffdb0955df34b8a9c8
ambkingdavid <kelvinwooldbridge@gmail.com>
2022-12-05 17:44:07 +0100

    add task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,7 +25,6 @@ int create_file(const char *filename, char *text_content)
 
 	if (fd == -1)
 	{
-		printf("file cannot be created\n");
 		return (-1);
 	}
 
@@ -33,7 +32,6 @@ int create_file(const char *filename, char *text_content)
 
 	if (w == -1)
 	{
-		printf("file cannot be written\n");
 		return (-1);
 	}
 
