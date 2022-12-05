6c8f2f40edc288888c54fa3f6c7f8c2c8b31e85d
ambkingdavid <kelvinwooldbridge@gmail.com>
2022-12-05 17:47:45 +0100

    add task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,15 +18,15 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content == NULL)
-		n = 0;
-
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 	{
 		return (-1);
 	}
+
+	if (text_content == NULL)
+		return (1);
 
 	w = write(fd, text_content, n);
 
